5975633b76f9ad3617dec9399a6e3d751a615642
ajaySingh2615 <ajaysingh261526@gmail.com>
2025-04-01 12:56:22 +0530

    Add initial OOP concepts and project setup
    Introduce basic examples of encapsulation and inheritance in Java. Added project VCS configuration and placeholder class for further development.

---
+++ core-java/src/com/oops/InheritanceDemo.java
@@ -0,0 +1,16 @@
+package com.oops;
+
+public class InheritanceDemo {
+    public static void main(String[] args) {
+        int result = sum(10, 10);
+        System.out.println(result);
+    }
+
+    public static int sum(int a, int b) {
+        int sum = a + b;
+        return sum;
+    }
+
+}
+
+
---
+++ core-java/src/com/oops/encapsulation.java
@@ -0,0 +1,7 @@
+package com.oops;
+
+public class encapsulation {
+    int id;
+    String name;
+    String course;
+}

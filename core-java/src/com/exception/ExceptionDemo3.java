b2c9cc9a97b4e983ff47fc45b5282c9e5b05e784
ajaySingh2615 <ajaysingh261526@gmail.com>
2025-04-07 16:21:18 +0530

    stack understanding using exception handling

---
+++ core-java/src/com/exception/ExceptionDemo3.java
@@ -0,0 +1,23 @@
+package com.exception;
+
+public class ExceptionDemo3 {
+    public static void main(String[] args) {
+        m1();
+    }
+
+    public static void m1() {
+        m2();
+    }
+
+    public static void m2() {
+        m3();
+    }
+
+    public static void m3() {
+        m4();
+    }
+
+    public static void m4() {
+        System.out.println(10 / 0);
+    }
+}

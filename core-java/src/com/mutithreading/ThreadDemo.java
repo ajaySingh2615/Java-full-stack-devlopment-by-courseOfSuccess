e3eb9763e21754ff732232ee83d111ea68b69691
ajaySingh2615 <ajaysingh261526@gmail.com>
2025-04-14 22:34:02 +0530

    #thread started

---
+++ core-java/src/com/mutithreading/ThreadDemo.java
@@ -0,0 +1,24 @@
+package com.mutithreading;
+
+class MyThread extends Thread {
+    //task/job
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("child thread " + i);
+        }
+    }
+}
+
+public class ThreadDemo {
+    public static void main(String[] args) {
+        MyThread t = new MyThread();
+
+        t.start();
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println("mike " + i);
+        }
+    }
+}

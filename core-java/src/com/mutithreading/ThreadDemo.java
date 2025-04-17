c4156499b6519a0ad8aea75eadb62899dd4a84d6
ajaySingh2615 <ajaysingh261526@gmail.com>
2025-04-17 10:25:19 +0530

    #thread start() method

--- core-java/src/com/mutithreading/ThreadDemo.java
+++ core-java/src/com/mutithreading/ThreadDemo.java
@@ -5,7 +5,7 @@ class MyThread extends Thread {
 
     @Override
     public void run() {
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 5; i++) {
             System.out.println("child thread " + i);
         }
     }

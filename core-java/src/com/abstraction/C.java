c52e3ccc3b6a34082d8edadaa555f16f92f3bdee
ajaySingh2615 <ajaysingh261526@gmail.com>
2025-05-06 21:33:15 +0530

    Add interfaces A, B, and class C with abstraction logic
    Introduced interfaces A and B, both defining a `show()` method. Implemented class C that overrides `show()` from both interfaces. Updated Test.java to demonstrate the use of class C, commenting out prior code for Koyal.

---
+++ core-java/src/com/abstraction/C.java
@@ -0,0 +1,9 @@
+package com.abstraction;
+
+public class C implements A, B {
+
+    @Override
+    public void show() {
+        System.out.println("show C");
+    }
+}
--- core-java/src/com/abstraction/Test.java
+++ core-java/src/com/abstraction/Test.java
@@ -2,9 +2,12 @@ package com.abstraction;
 
 public class Test {
     public static void main(String[] args) {
-        Koyal k = new Koyal();
-        k.sound();
-        k.eat();
-        System.out.println(Animal.a);
+//        Koyal k = new Koyal();
+//        k.sound();
+//        k.eat();
+//        System.out.println(Animal.a);
+
+        C obj = new C();
+        obj.show();
     }
 }

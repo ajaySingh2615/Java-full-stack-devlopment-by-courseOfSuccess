c0a111b01de42af96fdc5a95fe992d4adb46b1dc
ajaySingh2615 <ajaysingh261526@gmail.com>
2025-05-14 11:53:26 +0530

    Add LinkedListDemo class with basic LinkedList operations

---
+++ core-java/src/com/collectionsDemo/LinkedListDemo.java
@@ -0,0 +1,14 @@
+package com.collectionsDemo;
+
+import java.util.LinkedList;
+
+public class LinkedListDemo {
+    public static void main(String[] args) {
+        LinkedList linkedList = new LinkedList<>();
+        linkedList.add(10);
+        linkedList.add(20);
+        linkedList.add(30);
+        
+
+    }
+}

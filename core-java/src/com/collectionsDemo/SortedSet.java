f5e116a6e1ceed58abedb7b30fe5aac83a404109
ajaySingh2615 <ajaysingh261526@gmail.com>
2025-05-25 01:04:50 +0530

    Add initial App class and README with project setup details

--- core-java/src/com/collectionsDemo/SortedSet.java
+++ core-java/src/com/collectionsDemo/SortedSet.java
@@ -1,7 +1,9 @@
 package com.collectionsDemo;
 
+import java.util.TreeSet;
+
 public class SortedSet {
     public static void main(String[] args) {
-        SortedSet sortedSet;
+        Sorted
     }
 }

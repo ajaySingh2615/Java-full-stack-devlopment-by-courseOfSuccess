006f9c31f6b42589477b2ac56db5b4ae59aff822
ajaySingh2615 <ajaysingh261526@gmail.com>
2025-05-31 20:41:55 +0530

    Add initial Tomcat configuration files for servlet deployment
    Introduce essential Tomcat configuration files (`server.xml`, `context.xml`, `logging.properties`, `tomcat-users.xml`) under a new `.smarttomcat/firstServlet/conf` directory. Includes setup for servlet deployment, logging, user roles, and application settings. Also added IntelliJ IDEA encoding and project-specific metadata.

--- servlet/firstSevlet/src/main/java/FirstServlet.java
+++ servlet/firstSevlet/src/main/java/FirstServlet.java
@@ -1,2 +1,16 @@
-public class FirstServlet {
+import jakarta.servlet.GenericServlet;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class FirstServlet extends GenericServlet {
+    @Override
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+        servletResponse.setContentType("text/html");
+        PrintWriter out = servletResponse.getWriter();
+        out.println("<h1>M1</h1>");
+    }
 }
